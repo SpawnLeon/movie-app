ffa7329f388ad5c203dfd37f0f020937c259ef93
Alexander Leonov <spawnleon@gmail.com>
2022-04-08 20:55:08 +0300

    init: start project movie-app

---
+++ app.js
@@ -0,0 +1,68 @@
+const SEARCH_BTN = document.querySelector('#search');
+SEARCH_BTN.addEventListener('keypress', async (evt) => {
+
+  if (evt.key === 'Enter') {
+    evt.preventDefault();
+    const { target } = evt;
+    const { value } = target;
+    const movies = await getMovies(value);
+    console.log(movies);
+    renderMovies(movies);
+  }
+});
+
+const getDiscoverMovies = async () => {
+  const res = await fetch(
+    'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c');
+  const { results } = await res.json();
+  return results;
+};
+
+const getMovies = async (query) => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=3fd2be6f0c70a2a598f084ddfb75487c`);
+  const { results } = await res.json();
+  return results;
+};
+
+const getRatingClass = (r) => {
+  if (r < 5) {
+    return 'movie__rating--red';
+  }
+  if (r < 8) {
+    return 'movie__rating--yellow';
+  }
+  return 'movie__rating--green';
+};
+
+const renderMovies = (movies) => {
+  const moviesWrapper = document.querySelector('.movies');
+
+  moviesWrapper.innerHTML = movies.map(movie => {
+    return `<div class="movie">
+    <img src="https://image.tmdb.org/t/p/w1280${movie.poster_path}"
+         alt="${movie.title}">
+    <div class="movie__info">
+    <div class="movie__header">
+    <h3>${movie.title}</h3>
+      <span class="movie__rating ${getRatingClass(
+      movie.vote_average)}">${movie.vote_average}</span>
+    </div>
+      
+      <div class="movie__detail">
+        <h3>Overview</h3>
+        ${movie.overview}
+      </div>
+    </div>
+   
+  </div>`;
+  }).join('');
+
+};
+
+const main = async () => {
+  const movies = await getDiscoverMovies();
+  renderMovies(movies);
+};
+
+main();
